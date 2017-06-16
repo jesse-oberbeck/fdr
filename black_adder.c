cbfc863a40aefa08a9ddbe88290575279304a121
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2017-06-16 13:23:18 -0400

    Added conversion to hex.

--- black_adder.c
+++ black_adder.c
@@ -2,23 +2,81 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <openssl/bn.h>
 
 void print_num_array(int * c)
 {
     int index = 0;
     for(; c[index] == 0; ++index);
-    //printf("INDEX: %d\n", index);
-    for(index; index < 512; ++index)
+    for(; index < 512; ++index)
     {
         printf("%d", c[index]);
     }
     puts("");
 }
 
+char * stringify(int * ints, char * string)
+{
+    int index = 0;
+    for(; ints[index] == 0; ++index);
+    for(; index < 512; ++index)
+    {
+        sprintf(string, "%s%d", string, ints[index]);
+    }
+    puts("");
+    return(string);
+}
+
+char * to_hex(char * string)
+{
+    BIGNUM * big_num = BN_new();
+    BN_dec2bn(&big_num, string);
+    return(BN_bn2hex(big_num));
+}
+
+int roman(char * string)
+{
+    int accumulator = 0;
+    char characters[7] = "MDCLXVI";
+    int vals[7] = {1000, 500, 100, 50, 10, 5, 1};
+    int occurences[7] = {0};
+    int prev_val = 1000;
+    int new_val = 0;
+
+    for(unsigned int i = 0; i < strlen(string); ++i)
+    {
+        for(unsigned int j = 0; j < 7; ++j)
+        {
+
+            if(string[i] == characters[j])
+            {
+
+                new_val = vals[j];
+                occurences[j] = occurences[j] + 1;
+                if(new_val > prev_val)
+                {
+                    return(0);
+                }
+
+                if(occurences[j] > 4)
+                {
+                    return(0);
+                }
+
+                else
+                {
+                    accumulator = accumulator + new_val;
+                }
+
+            }
+        }
+    }
+    return(accumulator);
+}
+
 int * adder(int * a, int * b, int * c)
 {
     //TODO: Figure out how to handle carry properly.
-    //puts("adder~");
     int carry = 0;
     int sum = 0;
     int start = 512 - 1;
@@ -43,15 +101,12 @@ void fibo(int fibno, int * a, int * b, int * c)
     while(fibno > 0)
     {
         c = adder(a, b, c);
-        //printf("line~\n");
-        print_num_array(c);
-        //puts("~line");
+
+        //print_num_array(c);
+
         memcpy(a, b, 512 * sizeof(int));
         memcpy(b, c, 512 * sizeof(int));
-        //print_num_array(c);
-        //printf("first: %d\n", c[511]);
-        //memset(c, 0, 512 * sizeof(int));
-        //printf("first: %d\n", c[511]);
+
         --fibno;
     }
 }
@@ -72,10 +127,11 @@ void make_array(char * string, int * num, int start)
 
 int main(void)
 {
-    char * test = "779";
+    //char * test = "779";
     int a[512] = {0};
     int b[512] = {0};
     int c[512] = {0};
+    char string[512] = {'\0'};
 
     int start = 512 - 1;
 
@@ -83,14 +139,14 @@ int main(void)
     //make_array("300", b, start);
     fibo(300, a, b, c);
 
-    int index = 0;
-    for(; c[index] == 0; ++index);
-    printf("INDEX: %d\n", index);
-    for(index; index < 512; ++index)
-    {
-        printf("%d", c[index]);
-    }
-    puts("");
+    printf("roman: %d\n", roman("MMMMCVIIII"));
+    stringify(c, string);
+    printf("stringify: %s\n", string);
+    char * bign = to_hex(string);
+    printf("bignum hex: %s\n", bign);
+    printf("fibo: ");
+    print_num_array(c);
+
 
 }
 
